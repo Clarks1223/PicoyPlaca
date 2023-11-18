4e6940d03f07795794077a8df45a77e1641eb0ff
Clarks1223 <pablo.uchuari@epn.edu.ec>
2023-11-18 13:40:42 -0500

    Logic for the program, done!

--- src/Components/InformationPlate.jsx
+++ src/Components/InformationPlate.jsx
@@ -3,35 +3,53 @@ const Information = ({ formData }) => {
     return <p>Los datos del formulario no están definidos.</p>;
   }
 
-  const number = formData.plate.substring(formData.plate.length - 1);
-  const dia = new Date(formData.date).getDay();
-  const hour = new time(formData.time).getHour();
-  var road = true;
+  // Get the last digit of the plate
+  const numberPlate = formData.plate.substring(formData.plate.length - 1);
+  console.log(numberPlate);
 
-  switch (number) {
-    case "1":
-    case "2":
-      dia == 0 ? (road = false) : (road = true);
-      break;
-    case "3":
-    case "4":
-      dia == 1 ? (road = false) : (road = true);
-      break;
-    case "5":
-    case "6":
-      dia == 2 ? (road = false) : (road = true);
-      break;
-    case "7":
-    case "8":
-      dia == 3 ? (road = false) : (road = true);
-      break;
-    case "9":
-      dia == 4 ? (road = false) : (road = true);
-      break;
-    case "0":
-      dia == 4 ? (road = false) : (road = true);
-      break;
-  }
+  const canRoad = () => {
+    // Get weekday
+    const day = new Date(formData.date).getDay();
+    console.log(day);
+
+    // Get hours and minutes
+    const [hour, minutes] = formData.time
+      .split(":")
+      .map((item) => Number(item));
+
+    // Condition for circulation time
+    const validTime =
+      (hour >= 6 && hour < 9) ||
+      (hour === 9 && minutes < 30) ||
+      (hour >= 16 && hour < 20);
+
+    switch (day) {
+      case 0:
+        return (numberPlate == "1" || numberPlate == "2") && validTime;
+      case 1:
+        return (numberPlate == "3" || numberPlate == "4") && validTime;
+      case 2:
+        return (numberPlate == "5" || numberPlate == "6") && validTime;
+      case 3:
+        return (numberPlate == "7" || numberPlate == "8") && validTime;
+      case 4:
+        return (numberPlate == "9" || numberPlate == "0") && validTime;
+      case 5:
+      case 6:
+        return false;
+    }
+  };
+
+  const weekdayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const weekDay = weekdayNames[new Date(formData.date).getDay()];
 
   return (
     <>
@@ -39,11 +57,21 @@ const Information = ({ formData }) => {
         <h3>License plate number: {formData.plate}</h3>
         <h3>Date: {formData.date}</h3>
         <h3>Time: {formData.time}</h3>
-        <h3>Last digit of plate: {number}</h3>
-        <h3>dia: {dia}</h3>
-        <p>{road ? "You are able to road" : "Be careful, you can not road"}</p>
+        <h3>Last digit of plate: {numberPlate}</h3>
+        <h3>Day: {weekDay}</h3>
+        <p>
+          {canRoad()
+            ? "Be careful, you cannot road at this moment."
+            : "You're ready to hit the road. Wishing you an incredible journey!"}
+        </p>
+        <p>
+          {weekDay == "Saturday" || weekDay == "Sunday"
+            ? "Remember that the PICO y PLACA doesn't work on the weekend"
+            : "Schedule: 6:00 to 9:30 and 14:00 to 20:00"}
+        </p>
       </section>
     </>
   );
 };
+
 export default Information;
